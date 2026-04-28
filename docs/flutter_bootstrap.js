8385659aa947d3ab46a6a14461788cf3d01dc5ed
Nadia <brahmi.nadia@gmail.com>
2026-04-28 22:11:19 +0100

    Invite Code

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3962170706" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1706946912" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
