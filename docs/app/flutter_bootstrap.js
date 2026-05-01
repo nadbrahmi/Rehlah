34227cc4eb99ce5ee782f8ef10b58031ff1a7f4d
Nadia <brahmi.nadia@gmail.com>
2026-05-01 22:48:05 +0100

    Deploy Sprint 2: Tamoxifen streak

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2613261816" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1226567458" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
